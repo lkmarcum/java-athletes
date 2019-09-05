76fdbd766aa4cc34d95c4882e9c1d563f67e5c12
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2019-09-05 14:29:20 -0500

    set up background processor and injectors

---
+++ athlete/src/com/lambdaschool/solution/Athlete.java
@@ -0,0 +1,9 @@
+package com.lambdaschool.solution;
+
+public class Athlete
+{
+    public void displayAthlete(String sport, String position)
+    {
+        System.out.println(sport + position);
+    }
+}
